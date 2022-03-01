6cbe77cc65d7ed61625821f451936c9162bbef64
paqpac <94620920+paqpac@users.noreply.github.com>
2022-03-02 07:21:46 +0900

    新規登録・ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :first_name, :last_name, :kana_first_name, :kana_last_name, :birthday, presence: true
 end
