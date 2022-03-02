cff16773b5897cf680dceb76e9ddfa9a3a4a65b6
paqpac <94620920+paqpac@users.noreply.github.com>
2022-03-02 22:09:58 +0900

    全角、英数、入力制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :first_name, :last_name, :kana_first_name, :kana_last_name, :birthday, presence: true
+  validates :nickname, :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name ,:last_name, :kana_first_name, :kana_last_name
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を使用してください'
+
 end
