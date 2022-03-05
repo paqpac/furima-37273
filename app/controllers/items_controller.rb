863e19a73d152b079dc4229a6daab56d38f22b7c
paqpac <94620920+paqpac@users.noreply.github.com>
2022-03-05 16:08:40 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
   end
-
+  
   def new
     @item = Item.new
   end
   
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,14 @@ class Item < ApplicationRecord
   has_many :comments
   has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
+  validates :name, :introduction, presence: true
+  validates :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :delivery_time, numericality: { other_than: 1 , message: "カテゴリーを選択してください"}
 end
