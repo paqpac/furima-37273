9ce723f6d50836c8c9d15e4f54a9094497ab3efd
paqpac <94620920+paqpac@users.noreply.github.com>
2022-03-05 09:29:25 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:image)
+  end
 end
