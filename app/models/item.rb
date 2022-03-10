4ab1daaffdcf4590cbc750fce7a6bcb07006a25b
paqpac <94620920+paqpac@users.noreply.github.com>
2022-03-10 11:10:13 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   # has_many :comments
-  has_one :purchase
+  # has_one :purchase
   has_one_attached :image
   belongs_to :category
   belongs_to :item_status
@@ -10,8 +10,9 @@ extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :delivery_time
 
-  validates :name, :introduction, presence: true
-  validates :shipping_cost_id, :prefecture_id, :delivery_time_id,:category_id, :item_status_id,numericality: { other_than: 1 , message: "カテゴリーを選択してください"}
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :introduction, presence: true, length: { maximum: 1000 }
+  validates :shipping_cost_id, :prefecture_id, :delivery_time_id,:category_id, :item_status_id,numericality: { other_than: 1 , message: "リストから選択してください"}
   validates :image, presence: true
 
   with_options presence: true, format: { with: /\d/, message: '半角数値のみ使用してください' } do
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name          {Faker::Lorem.words}
+    introduction  {Faker::Lorem.sentence}
+    category_id   { 2 }
+    item_status_id   { 2 }
+    shipping_cost_id { 2 }
+    prefecture    { Prefecture.all.sample }
+    delivery_time_id {2}#{ DeliveryTime.all.sample }
+    price         {Faker::Number.between(from: 300, to: 9999999)}
     
+    association :user
+    
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test0.png'),filename: 'test0.png')
+    end
   end
 end
