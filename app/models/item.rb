6bf11f656921a23160b7006c5a23eebda218f531
paqpac <94620920+paqpac@users.noreply.github.com>
2022-03-12 16:45:41 +0900

    商品詳細表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   # has_many :comments
-  # has_one :purchase
+  has_one :purchase
   has_one_attached :image
   belongs_to :category
   belongs_to :item_status
