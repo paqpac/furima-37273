180f3ddc21e7ac1f992af649a14c9cc20f80b1d0
paqpac <94620920+paqpac@users.noreply.github.com>
2022-03-12 13:34:32 +0900

    商品一覧表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.all.order("created_at DESC")
   end
   
   def new
@@ -16,7 +17,7 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
+  
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :delivery_time_id,:price ).merge(user_id: current_user.id)
